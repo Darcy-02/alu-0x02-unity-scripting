2aaf6f9a01b7b60ae9c87fc88c5d22ef46a2d55e
unknown <d.mbanza@alustudent.com>
2026-09-12 16:32:56 -0700

    4

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+
+		if(other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            //add score
+            Debug.Log("the coins collected:");
+        }
+		
+	}
+}
